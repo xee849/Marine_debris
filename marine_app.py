388b7774f179926bf2934fee034f38716221af90
Zeeshan Hameed <zeeshan.hameed@aircod.com>
2024-12-23 17:54:00 +0500

    align3 header

--- marine_app.py
+++ marine_app.py
@@ -19,7 +19,7 @@ def detect_pol(image):
     
 def main():
     st.image("22223.png",use_container_width=True)
-    st.header("DETECTION")
+    st.header(">>>>>>> POLUTION DETECTION <<<<<<<<")
     st.sidebar.title("POLLUTION DETECTION")
     image = st.sidebar.file_uploader("Upload an Image",type=['jpg','png','jpeg'])
     if image:
